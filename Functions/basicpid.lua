14b2ce9e5ceef8bee0fab082062d2ae933508b2b
Icewav3 <coledorman12@gmail.com>
2022-06-01 19:37:06 -0700

    I MADE A PID POG

--- Functions/basicpid.lua
+++ Functions/basicpid.lua
@@ -1,8 +1,21 @@
-function pid(setpoint, current, p, i, d)
+pid = {
+    prev_err=prev_err or 0;
+    p_out=p_out or 0;
+    i_out=i_out or 0;
+    d_out=d_out or 0
+}
+function pid:run(setpoint, current, p, i, d)
     err = setpoint - current
-    p_out = err * p
-    i_out = err * i
-    d_out = err * d
-    output = p_out + i_out + d_out
-    return output
+    pid.p_out = err * p
+    pid.i_out = (err * i) + pid.i_out
+    pid.d_out = (err - pid.prev_err) * d
+    pid.prev_err = err
+    out = pid.p_out + pid.i_out + pid.d_out
+    return out
+end
+function onTick()
+    Setpoint = input.getNumber(1)
+    Current = input.getNumber(2)
+    throttle = pid:run(Setpoint, Current, 0.2, 0.0001, 0.1)
+    output.setNumber(1,throttle)
 end
