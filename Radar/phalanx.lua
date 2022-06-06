e7ae2d4997255e946faba5e57d5e82594c100f95
Icewav3 <coledorman12@gmail.com>
2022-06-06 15:09:39 -0700

    radar start

---
+++ Radar/phalanx.lua
@@ -0,0 +1,52 @@
+function onTick()
+    R1=input.getBool(1)
+    T1dist=input.getNumber(1)
+    T1azim=input.getNumber(2)
+    T1elev=input.getNumber(3)
+    T1time=input.getNumber(4)
+
+    R2=input.getBool(2)
+    T2dist=input.getNumber(5)
+    T2azim=input.getNumber(6)
+    T2elev=input.getNumber(7)
+    T2time=input.getNumber(8)
+
+    R3=input.getBool(3)
+    T3dist=input.getNumber(9)
+    T3azim=input.getNumber(10)
+    T3elev=input.getNumber(11)
+    T3time=input.getNumber(12)
+
+    R4=input.getBool(4)
+    T4dist=input.getNumber(13)
+    T4azim=input.getNumber(14)
+    T4elev=input.getNumber(15)
+    T4time=input.getNumber(16)
+
+    R5=input.getBool(5)
+    T5dist=input.getNumber(17)
+    T5azim=input.getNumber(18)
+    T5elev=input.getNumber(19)
+    T5time=input.getNumber(20)
+
+    R6=input.getBool(6)
+    T6dist=input.getNumber(21)
+    T6azim=input.getNumber(22)
+    T6elev=input.getNumber(23)
+    T6time=input.getNumber(24)
+
+    R7=input.getBool(7)
+    T7dist=input.getNumber(25)
+    T7azim=input.getNumber(26)
+    T7elev=input.getNumber(27)
+    T7time=input.getNumber(28)
+
+    R8=input.getBool(8)
+    T8dist=input.getNumber(29)
+    T8azim=input.getNumber(30)
+    T8elev=input.getNumber(31)
+    T8time=input.getNumber(32)
+
+    output.setNumber(1,top_rotor)
+    output.setNumber(2,btm_rotor)
+end
