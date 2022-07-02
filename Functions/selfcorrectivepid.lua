73841bd57c40b6937d1034947a15e3c7113827a9
Icewav3 <coledorman12@gmail.com>
2022-07-01 18:43:33 -0700

    minor work

---
+++ Functions/selfcorrectivepid.lua
@@ -0,0 +1,34 @@
+function PID(multi)
+    return {
+        p = p,
+        i = i,
+        d = d,
+        i_out = 0,
+        prev_err = 0,
+        run = function(self, min_setpoint, setpoint, current)
+            if setpoint > min_setpoint then
+                if current > setpoint * 0.1 + setpoint then
+                    self.i_out = 0
+                elseif current < setpoint * 0.1 setpoint then
+
+                end
+                err = setpoint - current
+                p_out = err * self.p
+                self.i_out = (err * self.i) + self.i_out
+                d_out = (err - self.prev_err) * self.d
+                self.prev_err = err
+                out = p_out + self.i_out + d_out
+            end
+            return out
+        end
+    }
+end
+--constants
+pid1=PID(0.2,0.0001,0.1)
+
+function onTick()
+    Setpoint = input.getNumber(1)
+    Current = input.getNumber(2)
+    throttle = pid1:run(2.5, Setpoint, Current)
+    output.setNumber(1,throttle)
+end
---
+++ Steering assists/waterjet_control.lua
@@ -0,0 +1,9 @@
+function onTick()
+    ws=getNumber(1)
+    ad=getNumber(2)
+    lr=getNumber(3)
+    --code here
+    output.setNumber(1,ws_out)
+    output.setNumber(2,ad_out)
+    output.setNumber(3,lr_out)
+end
